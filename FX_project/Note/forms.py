19236df05b7871e936905a8df49309dcc643fbcf
h-akira <79441679+h-akira@users.noreply.github.com>
2023-08-20 03:57:29 +0900

    update

--- FX_project/Note/forms.py
+++ FX_project/Note/forms.py
@@ -5,3 +5,6 @@ class ChartForm(forms.ModelForm):
   class Meta:
     model = ChartTable
     fields = ("user", 'name', 'pair', 'rule',"standard_datetime", "minus_delta", "plus_delta", "memo")
+    widgets = {
+      'memo': forms.Textarea(attrs={'rows': 10, 'cols': 50}),
+    }
